9b15c4e8cde85d6da0b2bb08125d7c2ff031ed23
Razertory <razertory@163.com>
2020-01-08 17:38:41 +0800

    add: functor description

--- src/main/scala/fpinscala/Functional.scala
+++ src/main/scala/fpinscala/Functional.scala
@@ -1,18 +1,20 @@
 package fpinscala
 
 object Functional {
-  // Monoid(幺半群) 由三个元素构成
-  // 1. 一个类型 A
-  // 2. 符合结合律的二元操作 op。例如，对于 x: A, y: A, z: A
-  //    op(op(x, y), z) 和 op(x, op(y, z)) 是等价的。意思是在 Scala 中 op(op(x, y), z) == op(x, op(y, z))
-  // 3. 单位元 zero: A。
-  //    对于整数加法操作，单位元就是 0；
-  //    对于整数乘法操作，单位元就是 1；
-  //    对于字符串拼接操作，单位元就是空字符串 "";
-  //    对于数组拼接操作，单位元就是空数组 Nil
-  // 简化一下就是，Monoid 是一个类型，一个该类型满足结合律的二元操作和一个该类型的单位元素
-  // 再简化就是，Monoid 是一个类型和一个类型的实现法则
-  // 在 Scala 中 Monoid 可以用一个 trait 表示
+  /**
+   Monoid(幺半群) 由三个元素构成
+   1. 一个类型 A
+   2. 符合结合律的二元操作 op。例如，对于 x: A, y: A, z: A
+      op(op(x, y), z) 和 op(x, op(y, z)) 是等价的。意思是在 Scala 中 op(op(x, y), z) == op(x, op(y, z))
+   3. 单位元 zero: A。
+      对于整数加法操作，单位元就是 0；
+      对于整数乘法操作，单位元就是 1；
+      对于字符串拼接操作，单位元就是空字符串 "";
+      对于数组拼接操作，单位元就是空数组 Nil
+   简化一下就是，Monoid 是一个类型，一个该类型满足结合律的二元操作和一个该类型的单位元素
+   再简化就是，Monoid 是一个类型和一个类型的实现法则
+   在 Scala 中 Monoid 可以用一个 trait 表示
+   */
   trait Monoid[A] {
     def op(a1: A, b2: A): A
     def zero: A
@@ -79,20 +81,34 @@ object Functional {
   }
 
 
-  // 使用类型构造器
+  /**
+   * Functor 是一个计算语境（computational context）
+   * 说明了如果给我一个从 A 映到 B 的函数，以及一个装了 A 的盒子，我会回给你一个装了 B 的盒子。
+   * 就好像用这个函数将每个元素都转成 B 一样。
+   * Functor Law
+   * 1. 当我们用 id 来 map over 一个 functor 的时候，他的结果应该跟只对 functor 调用 id 的结果一样。
+   * 2. 先将两个函数合成并将结果 map over 一个 functor 的结果，应该跟先将第一个函数 map over 一个 functor，再将第二个函数 map over 那个 functor 的结果是一样的
+   */
   trait Functor[F[_]] {
-    def map[A, B](fa: F[A])(f: A => B): F[B]
-
-    // 这里的类型构造器 List 本质上就是一个 Functor
-    val listFunctor = new Functor[List] {
-      override def map[A, B](fa: List[A])(f: A => B): List[B] = fa map f
-    }
-
-    // F[A, B] 是一个 Functor
-    def distribute[A, B](fab: F[(A, B)]): (F[A], F[B]) = ???
+    def fmap[A, B](a: F[A])(f: (A => B))
   }
 
-  def map[A, B](oa: Option[A])(f: A => B): Option[A] = ???
+  // List[A] => (A => B) => List[B]
+  val listFunctor = new Functor[List] {
+    override def fmap[A, B](l: List[A])(f: (A => B)) = {
+      l.map(f)
+    }
+  }
+
+  // Option[A] => (A => B) => Option[B]
+  val optionFunctor = new Functor[Option] {
+    override def fmap[A, B](a: Option[A])(f: A => B): Unit = {
+      a match {
+        case None    => None
+        case Some(a) => Some(f(a))
+      }
+    }
+  }
 
   trait Monad[F[_]] {
 
