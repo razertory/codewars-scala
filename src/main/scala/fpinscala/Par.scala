fd366567cf6c8a3a6d9602b34decdc7ab43f6b44
Razertory <razertory@163.com>
2020-01-29 11:43:01 +0800

    disable example

--- src/main/scala/fpinscala/Par.scala
+++ src/main/scala/fpinscala/Par.scala
@@ -96,16 +96,18 @@ object Par {
   implicit def toParOps[A](p: Par[A]): ParOps[A] = new ParOps(p)
 
   class ParOps[A](p: Par[A]) {
+  }
 
+  def main(args: Array[String]): Unit = {
   }
 }
 
-object Examples {
-  def sum(ints: IndexedSeq[Int]): Int = // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists, these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
-    if (ints.size <= 1)
-      ints.headOption getOrElse 0 // `headOption` is a method defined on all collections in Scala. We saw this function in chapter 3.
-    else {
-      val (l,r) = ints.splitAt(ints.length/2) // Divide the sequence in half using the `splitAt` function.
-      sum(l) + sum(r) // Recursively sum both halves and add the results together.
-    }
-}
+//object Examples {
+//  def sum(ints: IndexedSeq[Int]): Int = // `IndexedSeq` is a superclass of random-access sequences like `Vector` in the standard library. Unlike lists, these sequences provide an efficient `splitAt` method for dividing them into two parts at a particular index.
+//    if (ints.size <= 1)
+//      ints.headOption getOrElse 0 // `headOption` is a method defined on all collections in Scala. We saw this function in chapter 3.
+//    else {
+//      val (l,r) = ints.splitAt(ints.length/2) // Divide the sequence in half using the `splitAt` function.
+//      sum(l) + sum(r) // Recursively sum both halves and add the results together.
+//    }
+//}
